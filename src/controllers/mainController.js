d6faff8024d906064015fb2f433a8aa5669ea7c8
EmilianaABaglioni <emi.baglioni@outlook.com>
2022-06-27 23:30:16 -0300

    Mod clase 17/6

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../db/productsDataBase.json'); 
-const readJsonFile = (path) => {     
+const productsFilePath = path.join(__dirname, '../db/productsDataBase.json');
+const readJsonFile = (path) => {
     return JSON.parse(fs.readFileSync(path, 'utf-8'));
-}   
+}
 
 const controller = {
-    index: (req,res) => {
-    const products = readJsonFile(productsFilePath);
-       res.render("home", { products });
+    index: (req, res) => {
+        const products = readJsonFile(productsFilePath);
+        const bestseller = products.filter(product => product.categoria == "bestsellers").slice(0, 6);
+        const nuevosLanzamientos = products.filter(product => product.categoria == "nuevos-lanzamientos").slice(0, 6);
+
+        res.render("home", { bestseller, nuevosLanzamientos, products });
     }
 };
 
