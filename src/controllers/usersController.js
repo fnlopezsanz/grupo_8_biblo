ff8dea7271b631dfac0bb7fba76649fcd8e230bb
fnlopezsanz <facundonahuel.ls@gmail.com>
2022-07-13 17:38:52 -0300

    instalo multer y lo aplico al router de users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,18 +1,35 @@
 const fs = require("fs");
 const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../db/productsDataBase.json');
+const usersFilePath = path.join(__dirname, '../db/users.json');
 const readJsonFile = (path) => {
-  return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+  return JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 }
 
 const usersController = {
     login: (req,res) => {
         res.render("users/login");
     },
+
     register: (req,res) => {
         res.render("users/register");
     },
+
+    store: (req, res) => {
+    const users = readJsonFile(usersFilePath);
+    const user = {
+      id: users[users.length - 1].id + 1,
+      firstname: req.body.nombre,
+      lastname: req.body.apellido,
+      email: req.body.email,
+      password: req.body.contraseña,
+      image: req.file?.filename || "image-default.jpg"
+    };
+    users.push(user);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+    return res.redirect("/")
+    },
+
     carrito: (req, res) => {
         res.render("users/carrito")
     }
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+let multerDiskStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    let folder = path.join(__dirname, '../public/img/users');
+    cb(null, folder);
+  },
+  filename: (req, file, cb) => {
+    let imageName = 'user-' + Date.now() + path.extname(file.originalname);
+    cb(null, imageName);
+    }
+});
+
+let fileUpload = multer({ storage: multerDiskStorage });
 
 const usersController = require("../controllers/usersController.js");
 
 router.get("/login", usersController.login);
 
 router.get("/register", usersController.register);
+
+router.post("/store", fileUpload.single('image'), usersController.store);
 
 router.get("/carrito", usersController.carrito);
 
