596e843a1c066294c07b1fbcad35f8b5937b7a14
fnlopezsanz <facundonahuel.ls@gmail.com>
2022-07-17 14:36:15 -0300

    Instalo y configuro express-validator para el registro de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const path = require("path");
-
 const usersFilePath = path.join(__dirname, '../db/users.json');
 const readJsonFile = (path) => {
   return JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 }
+const { validationResult } = require('express-validator');
 
 const usersController = {
     login: (req,res) => {
@@ -16,18 +16,27 @@ const usersController = {
     },
 
     store: (req, res) => {
-    const users = readJsonFile(usersFilePath);
-    const user = {
-      id: users[users.length - 1].id + 1,
-      firstname: req.body.Nombre,
-      lastname: req.body.Apellido,
-      email: req.body.Email,
-      password: req.body.Password,
-      image: req.file?.filename || "image-default.jpg"
-    };
-    users.push(user);
-    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
-    return res.redirect("/")
+      const resultValidation = validationResult(req);
+
+      if (resultValidation.errors.length > 0) {
+        return res.render('users/register', {
+            errors: resultValidation.mapped(),
+            oldData: req.body
+        });
+      }
+
+      const users = readJsonFile(usersFilePath);
+      const user = {
+        id: users[users.length - 1].id + 1,
+        firstname: req.body.Nombre,
+        lastname: req.body.Apellido,
+        email: req.body.Email,
+        password: req.body.Password,
+        image: req.file?.filename || "image-default.jpg"
+      };
+      users.push(user);
+      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+      return res.redirect("/")
     },
 
     carrito: (req, res) => {
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const { body } = require('express-validator');
+const usersController = require("../controllers/usersController.js");
 
 let multerDiskStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -16,13 +18,33 @@ let multerDiskStorage = multer.diskStorage({
 
 let fileUpload = multer({ storage: multerDiskStorage });
 
-const usersController = require("../controllers/usersController.js");
+const validations = [
+  body('Nombre').notEmpty().withMessage('Debe introducir un nombre'),
+  body('Apellido').notEmpty().withMessage('Debe introducir un apellido'),
+  body('Email').notEmpty().withMessage('Debe introducir un correo electrónico').bail()
+  .isEmail().withMessage('Debe introducir un formato de correo válido'),
+  body('Password').notEmpty().withMessage('Debe introducir una contraseña'),
+  body('image').custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = ['.jpg', '.png'];
+    
+    if (!file) {
+      throw new Error('Debe seleccionar una imagen');
+    } else {
+      let fileExtension = path.extname(file.originalname);
+      if(!acceptedExtensions.includes(fileExtension)) {
+        throw new Error('Las extensiones de archivo permitidas son: ' + acceptedExtensions.join(', '));
+      }
+    }
+    return true
+  })
+];
 
 router.get("/login", usersController.login);
 
 router.get("/register", usersController.register);
 
-router.post("/store", fileUpload.single('image'), usersController.store);
+router.post("/store", fileUpload.single('image'), validations, usersController.store);
 
 router.get("/carrito", usersController.carrito);
 
