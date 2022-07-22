99e907facd84e2df203417a0766a66eab409e909
EmilianaABaglioni <emi.baglioni@outlook.com>
2022-07-22 20:52:15 -0300

    recordame ok

--- src/middlewares/userLogMiddleware.js
+++ src/middlewares/userLogMiddleware.js
@@ -1,8 +1,19 @@
 module.exports = (req, res, next) => {
+    const User = require('../models/User');
+
     res.locals.isLogged = true;
+    
+    const emailInCookie = req.cookies.userData;
+    const userFromCookie = User.findByFile('email', emailInCookie);
+
+    if(userFromCookie) {
+        req.session.userLogged = userFromCookie;
+    }
+
     if (req.session && req.session.userLogged) {
         res.locals.isLogged = false;
         res.locals.userLogged = req.session.userLogged
     }
+
     next();
 }
