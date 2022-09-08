76af7ae1f2a4cd62b0d1958f626029685944c27d
fnlopezsanz <facundonahuel.ls@gmail.com>
2022-09-08 17:44:11 -0300

    validación front imagen register a medias

--- src/public/js/validateRegisterFront.js
+++ src/public/js/validateRegisterFront.js
@@ -5,14 +5,17 @@ window.addEventListener('load', function() {
   const apellido = document.querySelector('#apellido');
   const email = document.querySelector('#email');
   const password = document.querySelector('#password');
+  const image = document.querySelector('#image');
   
 //Captura de divs de errores del formulario
   const nombreError = document.querySelector('#nombreError');
   const apellidoError = document.querySelector('#apellidoError');
   const emailError = document.querySelector('#emailError');
   const passwordError = document.querySelector('#passwordError');
+  const imageError = document.querySelector('#imageError');
 
 // Funciones
+// Mínimo 2 caracteres para Nombre y Apellido
   function validateLength(valueInput, divInput, divError) {
     if (valueInput.length < 2) {
       showError(divInput, divError);
@@ -21,19 +24,22 @@ window.addEventListener('load', function() {
     }
   }
 
+  // Muestra borde rojo del input y div con texto de error para Nombre y Apellido
   function showError(divInput, divError) {
     divInput.style.border = "1px solid coral";
     divError.innerHTML = `Debe introducir un ${divInput.id} de al menos 2 caracteres`;
   }
   
+  // Esconde borde rojo del input y div con texto de error para Nombre y Apellido
   function hideError(divInput, divError) {
     divInput.style.border = "";
     divError.innerHTML = "";
   }
   
+  // Valida formato válido de email a partir de expresiones regulares
   function validateEmail(valueInput, divInput) {
     let regExpEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
-
+    
     if(regExpEmail.test(valueInput)) {
       divInput.style.border = "";
       emailError.innerHTML = "";
@@ -42,9 +48,10 @@ window.addEventListener('load', function() {
       emailError.innerHTML = "Debe introducir un formato de correo válido"
     }
   }
-
+  
+  // Valida password a partir de expresiones regulares. Debe contener 1 may, 1 min, 1 número y 1 caracter especial
   function validatePassword(valueInput, divInput) {
-    let regExpPassword = /^(?=[\x21-\x7E]*[0-9])(?=[\x21-\x7E]*[A-Z])(?=[\x21-\x7E]*[a-z])(?=[\x21-\x7E]*[\x21-\x2F|\x3A-\x40|\x5B-\x60|\x7B-\x7E])[\x21-\x7E]{6,}$/
+    let regExpPassword = /^(?=[\x21-\x7E]*[0-9])(?=[\x21-\x7E]*[A-Z])(?=[\x21-\x7E]*[a-z])(?=[\x21-\x7E]*[\x21-\x2F|\x3A-\x40|\x5B-\x60|\x7B-\x7E])[\x21-\x7E]{8,}$/
 
     if (regExpPassword.test(valueInput)) {
       divInput.style.border = "";
@@ -55,6 +62,19 @@ window.addEventListener('load', function() {
     }
   }
 
+  function validateImage(valueInput) {
+    let regExpExtension = /[^\\s]+(\\.(jpeg|png|gif|jpg))$/
+    console.log(regExpExtension.test(valueInput), valueInput)
+    /* if(!regExpExtension.exec(valueInput)) {
+      divInput.style.border = "1px solid coral";
+      imageError.innerHTML = "Las extensiones permitidas son .gif, .jpg, .jpeg y .png";
+      valueInput = "";
+    } else {
+      divInput.style.border = "";
+      imageError.innerHTML = "";
+    }*/
+  } 
+  validateImage(image.value)
 //Eventos
   nombre.addEventListener('blur', function() {
     validateLength(nombre.value, nombre, nombreError);
@@ -70,6 +90,10 @@ window.addEventListener('load', function() {
   
   password.addEventListener('blur', function() {
     validatePassword(password.value, password)
+  })
+  
+  image.addEventListener('change', function() {
+    validateImage(image.value)
   })
  
 })
